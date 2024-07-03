475aa19075ee6eb089863c1e442ba2b748fad7d9
pablot95 <pablo.travi95@gmail.com>
2024-07-03 14:46:32 -0300

    Primera preentrega

---
+++ Java.js
@@ -0,0 +1,34 @@
+
+let nombre = prompt("Por favor ingresa su nombre")
+alert ("Bienvenido" + " " + nombre + "!");
+console.log("hola" + " " + nombre)
+
+let artista = prompt("¿Desea comprar entradas para concierto de 'taylor swift' o de 'metallica'?")
+
+while (artista != "taylor swift" && artista != "metallica"){
+    alert("Porfavor ingrese una respuesta correcta")
+    artista = prompt("Desea comprar entradas para concierto de taylor swift o de metallica")
+}
+
+console.log(nombre + " " + "comprara entradas para " + " " + artista)
+
+if(artista == "metallica"){
+    let edad = Number(prompt("Porfavor indique su edad")) 
+    if(edad >= 18){
+        let cantidadentradas1 = prompt("La entrada cuesta un total de $50.000 ¿Cuantas entradas desea comprar?")
+        let montototal = `${cantidadentradas1*50000}`
+        alert( "El monto total a pagar es de =" + " " + "$" + montototal)
+        console.log(nombre + " " + "Debe abonar un total de" + " " + "$" + montototal)
+    }else {
+        alert("solo mayores de edad pueden asistir al evento, lo sentimos")
+    }
+}
+
+if(artista == "taylor swift"){
+    let cantidadentradas2 = prompt("La entrada cuesta un total de $100.000 ¿Cuantas entradas desea comprar?")
+        let montototal = `${cantidadentradas2*100000}`
+        alert( "El monto total a pagar es de =" + " " + "$" + montototal)
+        console.log(nombre + " " + "debe abonar un total de" + " " + "$" + montototal)
+}
+
+alert ("¡Gracias por su visita!")  
